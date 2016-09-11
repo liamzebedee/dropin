09e772cd18609d3854d5497be60fdbb17ecc397c
Liam Edwards-Playne <liamzebedee@yahoo.com.au>
2016-09-11 16:57:37 +1000

    subject search API call works

--- api/main.js
+++ api/main.js
@@ -3,6 +3,11 @@ var assert = require('assert');
 
 const MONGO_URL = "mongodb://test:password@ds161255.mlab.com:61255/dbdropin";
 
+function isDigit(n) {
+    return Boolean([true, true, true, true, true, true, true, true, true, true][n]);
+}
+
+
 
 var mongoDatabase;
 // Connect to the db
@@ -10,6 +15,8 @@ MongoClient.connect(MONGO_URL, function(err, db) {
   if(!err) {
     console.log("We are connected");
     mongoDatabase = db;
+    mongoDatabase.collection('classes').createIndex({ subjectName: "text" })
+    
   }else{
     console.log("MongoDB connection error", err);
   }
@@ -80,7 +87,36 @@ router.get('/subjects/nearby', (req, res) => {
 });
 
 router.get('/subjects/search', (req, res) => {
+  let query = {
+    q: req.query.q
+    // q: "48023",
+    // q: "french",
+  }
 
+  let selector;
+
+  if(isDigit(query.q.charAt(0))) {
+    // Search for subject code
+    selector = {
+      subjectId: query.q
+    };
+
+  } else {
+    // Search for subject name
+    selector = {
+      $text: {
+          $search: query.q.toLowerCase(),
+          // $caseSensitive: false,
+          $language: "EN"
+        }
+    };
+  }
+
+  var subjects = mongoDatabase.collection('classes');
+  subjects.find(selector).toArray((err,re)=>{
+    res.send(re);
+  });
+  
 })
 
 router.get('/feedback', (req,res)=>{
